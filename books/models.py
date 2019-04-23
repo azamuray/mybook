6c39646dcd3677dce7373ff6cdad848c1e4cbf9b
ai-rage <azamat.murdalov@mail.ru>
2019-04-23 18:03:04 +0300

    add Book model

--- books/models.py
+++ books/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+	title = models.CharField(max_length=120)
+	author = models.CharField(max_length=120)
+	image = models.ImageField(
+		null=True, blank=True,
+		upload_to='images/',
+		verbose_name='Изображение'
+		)
+
+	def __str__(self):
+		return self.title
+
+	def get_absolute_url(self):
+		return reverse('news:post-detail', kwargs={'pk': self.pk})
