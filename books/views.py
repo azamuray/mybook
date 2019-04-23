e8d1713974447ddccfcbb5a77d7b564b21a52288
ai-rage <azamat.murdalov@mail.ru>
2019-04-23 18:45:54 +0300

    add books template

--- books/views.py
+++ books/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Book
 
-# Create your views here.
+def home(request):
+	context = {
+		'books': Book.objects.all()
+	}
+	return render(request, 'books/home.html', context)
